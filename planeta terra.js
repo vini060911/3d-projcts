1318195542606b7d6c35a2d1fe0d31707a4b254b
vini060911 <90809396+vini060911@users.noreply.github.com>
2021-09-16 13:52:36 -0300

    planeta terra.js
    Cria um planeta terra no javascript.

---
+++ planeta terra.js
@@ -0,0 +1,29 @@
+const
+geometry = new THREE.SphereGeometry(),
+textura = new THREE.TextureLoader().load( 'https://static.vecteezy.com/system/resources/previews/000/142/156/original/mapa-mundi-vector.jpg' ),
+cena = new THREE.TextureLoader().load( 'https://th.bing.com/th/id/R.e2c5c17b50b15da87ea1ae78f0538a7b?rik=WZV2O5c3FjexsA&riu=http%3a%2f%2ffarm9.staticflickr.com%2f8060%2f8263358842_a2748b2170_z.jpg&ehk=UWDC1bSwYQjyIH%2bGLuXhAbQ8eXQdb003w9WzRiPFzyc%3d&risl=&pid=ImgRaw&r=01' ),
+material = new THREE.MeshBasicMaterial( { map: textura } ),
+esfera = new THREE.Mesh( geometry, material )
+
+scene.add( esfera )
+scene.background = cena
+
+camera.position.z = 5;
+
+const animate = function () {
+  requestAnimationFrame( animate );
+
+	esfera.rotation.y += Math.PI/360;
+
+	renderer.render( scene, camera );
+};
+
+animate();
+document.getElementById("enviar").addEventListener("click", () => {
+  function ctrAnim(data) {
+    esfera.rotation.y += Math.PI/360;
+
+	  renderer.render( scene, camera );
+  }
+	ctrAnim
+})
